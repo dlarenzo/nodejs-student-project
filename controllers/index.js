9ea8624a077bde1feb68e86314caa9deb573a85f
LaRenzo <dlarenzo@outlook.com>
2024-02-01 10:15:40 -0700

    finished part-2

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,24 @@
-const awesomeFunction = (Req, res) => {
-  res.send("THIS IS AMAZING!");
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
+
+const awesomeFunction = (req, res, next) => {
+  res.json("THIS IS AMAZING!");
 };
 
-const tooeleTech = (Req, res) => {
-  res.send("Tooele Tech is Awesome!");
+const tooeleTech = (req, res, next) => {
+  res.json("Tooele Tech is Awesome!");
 };
 
-module.exports = { awesomeFunction, tooeleTech };
+const getAllStudents = async (req, res) => {
+  try {
+    const result = await mongodb.getDb().db().collection("students").find();
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { awesomeFunction, tooeleTech, getAllStudents };
